2e1bd2498ae101738eadf7bf7771d118e58991ee
Christian Yarwood <christianyarwood@gmail.com>
2025-04-05 19:12:38 +0100

    Delete read attribute from array

--- project_library/script.js
+++ project_library/script.js
@@ -1,25 +1,31 @@
+// Library of inputted books
 const myLibrary = [
-    {title:'harry potter',
-    author: 'Test',
-    pages: '300',
-    read: 'yes',
+    {
+        title:'harry potter',
+        author: 'J K Rowling',
+        pages: '300',
+        literature: 'Fiction',
     },
-    {title: 'lord of the rings',
-    author: 'christian',
-    pages: '300',
-    read: 'no',
+    {
+        title: 'lord of the rings',
+        author: 'christian',
+        pages: '1000',
+        literature: 'Non-Fiction',
     }
 ];
 
-function Book(title, author, pages, read, id) {
+// Book Constructor
+function Book(title, author, pages, literature) {
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read,
-    this.id = id;
+    this.literature = literature;
 }
 
+// Event: Add inputted books to the book array
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read, id);
     myLibrary.push(book);
 }
+
+// Event:

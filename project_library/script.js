7666c2d24b5571d7080c7bdd027432dc0d9a08ab
Christian Yarwood <christianyarwood@gmail.com>
2025-04-11 20:47:05 +0100

    change to queryselector for sidebar

--- project_library/script.js
+++ project_library/script.js
@@ -200,12 +200,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.querySelector("#mySidebar").style.width = "250px";
+    document.querySelector("#main").style.marginLeft = "250px";
   }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.querySelector("#mySidebar").style.width = "0";
+    document.querySelector("#main").style.marginLeft = "0";
+}
